4bc588d45e7e1ded96b40421fed2d02fe5ecdf37
elvinsavio <elvinsavio.lp@gmail.com>
2025-04-08 20:22:25 +0530

    fix: styles not being applied

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,81 +1,52 @@
 module.exports = {
-    purge: [],
-    darkMode: false, // or 'media' or 'class'
-    content: ['./templates/**/*.html', './templates/*.html'],
-    theme: {
-      extend: {
-        fontFamily: {},
-        colors: {
-          border: "hsl(var(--border))",
-          input: "hsl(var(--input))",
-          ring: "hsl(var(--ring))",
-          background: "hsl(var(--background))",
-          foreground: "hsl(var(--foreground))",
-          primary: {
-            DEFAULT: "hsl(var(--primary))",
-            foreground: "hsl(var(--primary-foreground))",
-          },
-          secondary: {
-            DEFAULT: "hsl(var(--secondary))",
-            foreground: "hsl(var(--secondary-foreground))",
-          },
-          destructive: {
-            DEFAULT: "hsl(var(--destructive) / <alpha-value>)",
-            foreground: "hsl(var(--destructive-foreground) / <alpha-value>)",
-          },
-          muted: {
-            DEFAULT: "hsl(var(--muted))",
-            foreground: "hsl(var(--muted-foreground))",
-          },
-          accent: {
-            DEFAULT: "hsl(var(--accent))",
-            foreground: "hsl(var(--accent-foreground))",
-          },
-          popover: {
-            DEFAULT: "hsl(var(--popover))",
-            foreground: "hsl(var(--popover-foreground))",
-          },
-          card: {
-            DEFAULT: "hsl(var(--card))",
-            foreground: "hsl(var(--card-foreground))",
-          },
-          sidebar: {
-            DEFAULT: "hsl(var(--sidebar-background))",
-            foreground: "hsl(var(--sidebar-foreground))",
-            primary: "hsl(var(--sidebar-primary))",
-            "primary-foreground": "hsl(var(--sidebar-primary-foreground))",
-            accent: "hsl(var(--sidebar-accent))",
-            "accent-foreground": "hsl(var(--sidebar-accent-foreground))",
-            border: "hsl(var(--sidebar-border))",
-            ring: "hsl(var(--sidebar-ring))",
-          },
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  content: ['./templates/**/*.html', './templates/*.html'],
+  theme: {
+    extend: {
+      fontFamily: {},
+      colors: {
+        border: "var(--border)",
+        input: "var(--input)",
+        ring: "hsl(var(--ring))",
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        primary: {
+          DEFAULT: "var(--primary)",
+          foreground: "var(--primary-foreground)",
         },
-        borderRadius: {
-          xl: "calc(var(--radius) + 4px)",
-          lg: "var(--radius)",
-          md: "calc(var(--radius) - 2px)",
-          sm: "calc(var(--radius) - 4px)",
+        secondary: {
+          DEFAULT: "var(--secondary)",
+          foreground: "var(--secondary-foreground)",
         },
-        keyframes: {
-          "accordion-down": {
-            from: { height: "0" },
-            to: { height: "var(--radix-accordion-content-height)" },
-          },
-          "accordion-up": {
-            from: { height: "var(--radix-accordion-content-height)" },
-            to: { height: "0" },
-          },
-          "caret-blink": {
-            "0%,70%,100%": { opacity: "1" },
-            "20%,50%": { opacity: "0" },
-          },
+        destructive: {
+          DEFAULT: "var(--destructive) / <alpha-value>",
+          foreground: "var(--destructive-foreground) / <alpha-value>",
         },
-        animation: {
-          "accordion-down": "accordion-down 0.2s ease-out",
-          "accordion-up": "accordion-up 0.2s ease-out",
-          "caret-blink": "caret-blink 1.25s ease-out infinite",
+        muted: {
+          DEFAULT: "var(--muted)",
+          foreground: "var(--muted-foreground)",
+        },
+        accent: {
+          DEFAULT: "var(--accent)",
+          foreground: "var(--accent-foreground)",
+        },
+        popover: {
+          DEFAULT: "var(--popover)",
+          foreground: "var(--popover-foreground)",
+        },
+        card: {
+          DEFAULT: "var(--card)",
+          foreground: "var(--card-foreground)",
         },
       },
+      borderRadius: {
+        xl: "calc(var(--radius) + 4px)",
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
     },
-    plugins: [require("tailwindcss-animate")],
-  }
+  },
+  plugins: [require("tailwindcss-animate")],
+}
